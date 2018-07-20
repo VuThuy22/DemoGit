de4aaa99f35c85360ab36a169420e3e35acd7949
ThuyVu <vthuy2222@gmail.com>
2018-07-20 14:31:00 +0700

    hh

---
+++ smsBroadcast_detail/app/src/main/java/vn/luckybets/broadcastsmsbanking/utils/TimeUtils.java
@@ -0,0 +1,23 @@
+package vn.luckybets.broadcastsmsbanking.utils;
+
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class TimeUtils {
+    public static String getTimeLocal()
+
+    {
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"),
+                Locale.getDefault());
+        Date currentLocalTime = calendar.getTime();
+        DateFormat date = new SimpleDateFormat("Z");
+        String localTime = date.format(currentLocalTime);
+        return localTime;
+    }
+
+}
